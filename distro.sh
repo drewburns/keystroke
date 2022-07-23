b40bdb722469efcd11892c36f820affb1f07acd3
drewburns <drewburnsbab@gmail.com>
2022-07-23 14:35:36 -0400

    node

--- distro.sh
+++ distro.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# curl "https://nodejs.org/dist/latest/node-${VERSION:-$(wget -qO- https://nodejs.org/dist/latest/ | sed -nE 's|.*>node-(.*)\.pkg</a>.*|\1|p')}.pkg" > "$HOME/Downloads/node-latest.pkg" && sudo installer -store -pkg "$HOME/Downloads/node-latest.pkg" -target "/"
+curl "https://nodejs.org/dist/latest/node-${VERSION:-$(wget -qO- https://nodejs.org/dist/latest/ | sed -nE 's|.*>node-(.*)\.pkg</a>.*|\1|p')}.pkg" > "$HOME/Downloads/node-latest.pkg" && sudo installer -store -pkg "$HOME/Downloads/node-latest.pkg" -target "/"
 cd ~/Desktop
 rm -rf ~/Desktop/keystroke
 git clone https://github.com/drewburns/keystroke.git keystroke
